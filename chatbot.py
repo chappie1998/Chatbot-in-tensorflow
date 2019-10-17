cb9cf7bcb04da6908eeaa816028d29aad60b76d1
Ankit Kumar Saini <35380717+ankitgc1@users.noreply.github.com>
2019-10-17 18:47:24 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,392 @@
+
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Art by Ankit>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+
+from __future__ import absolute_import, division, print_function
+
+# Import TensorFlow >= 1.10 and enable eager execution
+import tensorflow as tf
+
+print(tf.__version__)
+
+# if tf.__version__ is not '2.0.0-beta1':
+tf.compat.v1.enable_eager_execution()
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import numpy as np
+import unicodedata
+import re
+import os
+import time
+
+
+with open("data/encoder_data.txt", 'r') as f:
+    encoder_inputs = f.readlines()
+    encoder_inputs = encoder_inputs[:30000]
+
+with open("data/decoder_data.txt", 'r') as f:
+    decoder_inputs = f.readlines()
+    decoder_inputs = decoder_inputs[:30000]
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.rstrip().strip()
+
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<BOS> ' + w + ' <EOS>\n'
+    return w
+
+word2idx = {}
+idx2word = {}
+vocab = set()
+
+text = encoder_inputs + decoder_inputs
+
+for phrase in text:
+    vocab.update(phrase.split(' '))
+
+word2idx['<pad>'] = 0
+for index, word in enumerate(vocab):
+  word2idx[word] = index + 1
+
+for word, index in word2idx.items():
+  idx2word[index] = word
+
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+#input encoder_data
+input_tensor = [[word2idx[s] for s in encoder.split(' ')] for encoder in encoder_inputs]
+
+#output decoder_data
+target_tensor = [[word2idx[s] for s in decoder.split(' ')] for decoder in decoder_inputs]
+
+# Calculate max_length of input and output tensor
+# Here, we'll set those to the longest sentence in the dataset
+max_length_inp = max_length(input_tensor)
+max_length_tar = max_length(target_tensor)
+
+# Padding the input and output tensor to the maximum length
+input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor,
+                                                                 maxlen=max_length_inp,
+                                                                 padding='post')
+
+target_tensor = tf.keras.preprocessing.sequence.pad_sequences(target_tensor,
+                                                                  maxlen=max_length_tar,
+                                                                  padding='post')
+
+
+# Creating training and validation sets using an 80-20 split
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+vocab_size = len(word2idx) + 1
+print("VOCAB_SIZE:- ", vocab_size)
+print("max_length_inp:- ", max_length_inp)
+print("max_length_tar:- ", max_length_tar)
+
+# Show length
+print("input_tensor_train:- ", len(input_tensor_train))
+print("target_tensor_train:- ", len(target_tensor_train))
+print("input_tensor_val:- ", len(input_tensor_val))
+print("target_tensor_val:- ", len(target_tensor_val))
+
+max_length_inp = 30
+max_length_tar = 30
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 32
+N_BATCH = BUFFER_SIZE//BATCH_SIZE
+embedding_dim = 100
+units = 128
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+# GLOVE_DIR = path for glove.6B.100d.txt
+embeddings_index = {}
+f = open("glove.6B.100d.txt")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+# this time: embedding_dimention = 100d
+def embedding_matrix_creater(embedding_dimention, embeddings_index, word_index):
+    embedding_matrix = np.zeros((len(word_index) + 1, embedding_dimention))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+
+embedding_matrix = embedding_matrix_creater(embedding_dim, embeddings_index, word2idx)
+
+def embedding_layer_creater(VOCAB_SIZE, EMBEDDING_DIM, MAX_LEN, embedding_matrix):
+
+    embedding_layer = tf.keras.layers.Embedding(input_dim = VOCAB_SIZE,
+                                output_dim = EMBEDDING_DIM,
+                                input_length = MAX_LEN,
+                                weights = [embedding_matrix],
+                                trainable = False)
+    return embedding_layer
+
+embed_layer = embedding_layer_creater(vocab_size, embedding_dim, max_length_inp, embedding_matrix)
+embed_layer.build((None,))
+embed_layer.set_weights([embedding_matrix])
+
+
+
+def gru(units):
+  # If you have a GPU, we recommend using CuDNNGRU(provides a 3x speedup than GRU)
+  # the code automatically does that.
+  if tf.test.is_gpu_available():
+    return tf.keras.layers.CuDNNGRU(units,
+                                    return_sequences=True,
+                                    return_state=True,
+                                    recurrent_initializer='glorot_uniform')
+  else:
+    return tf.keras.layers.GRU(units,
+                               return_sequences=True,
+                               return_state=True,
+                               recurrent_activation='sigmoid',
+                               recurrent_initializer='glorot_uniform')
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+        super(Encoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        # self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        # self.embedding  = embed_layer()
+        self.gru = gru(self.enc_units)
+
+    def call(self, x, hidden, embed_layer):
+        x = embed_layer(x)
+        output, state = self.gru(x, initial_state = hidden)
+        return output, state
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.enc_units))
+
+class Decoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+        super(Decoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.dec_units = dec_units
+        # self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        # self.embedding  = embed_layer()
+        self.gru = gru(self.dec_units)
+        self.fc = tf.keras.layers.Dense(vocab_size)
+
+        # used for attention
+        self.W1 = tf.keras.layers.Dense(self.dec_units)
+        self.W2 = tf.keras.layers.Dense(self.dec_units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, x, hidden, enc_output, embed_layer):
+        # enc_output shape == (batch_size, max_length, hidden_size)
+
+        # hidden shape == (batch_size, hidden size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+        # we are doing this to perform addition to calculate the score
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+        # score shape == (batch_size, max_length, 1)
+        # we get 1 at the last axis because we are applying tanh(FC(EO) + FC(H)) to self.V
+        score = self.V(tf.nn.tanh(self.W1(enc_output) + self.W2(hidden_with_time_axis)))
+
+        # attention_weights shape == (batch_size, max_length, 1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * enc_output
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = embed_layer(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+
+        # output shape == (batch_size * 1, hidden_size)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # output shape == (batch_size * 1, vocab)
+        x = self.fc(output)
+
+        return x, state, attention_weights
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.dec_units))
+
+encoder = Encoder(vocab_size, embedding_dim, units, BATCH_SIZE)
+decoder = Decoder(vocab_size, embedding_dim, units, BATCH_SIZE)
+
+# if tf.__version__=='2.0.0-beta1':
+#     optimizer = tf.keras.optimizers.Adam()
+#     loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+#         from_logits=True, reduction='none')
+#
+#     def loss_function(real, pred):
+#       mask = tf.math.logical_not(tf.math.equal(real, 0))
+#       loss_ = loss_object(real, pred)
+#
+#       mask = tf.cast(mask, dtype=loss_.dtype)
+#       loss_ *= mask
+#
+#       return tf.reduce_mean(loss_)
+# else:
+
+optimizer = tf.train.AdamOptimizer()
+
+def loss_function(real, pred):
+  mask = 1 - np.equal(real, 0)
+  loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=real, logits=pred) * mask
+  return tf.reduce_mean(loss_)
+
+checkpoint_dir = './training_checkpoint_test'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                 encoder=encoder,
+                                 decoder=decoder)
+
+EPOCHS = 100
+
+for epoch in range(EPOCHS):
+    start = time.time()
+
+    hidden = encoder.initialize_hidden_state()
+    total_loss = 0
+
+    for (batch, (inp, targ)) in enumerate(dataset):
+        loss = 0
+
+        with tf.GradientTape() as tape:
+            enc_output, enc_hidden = encoder(inp, hidden, embed_layer)
+
+            dec_hidden = enc_hidden
+
+            dec_input = tf.expand_dims([word2idx['<BOS>']] * BATCH_SIZE, 1)
+
+            # Teacher forcing - feeding the target as the next input
+            for t in range(1, targ.shape[1]):
+                # passing enc_output to the decoder
+                predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output, embed_layer)
+
+                loss += loss_function(targ[:, t], predictions)
+
+                # using teacher forcing
+                dec_input = tf.expand_dims(targ[:, t], 1)
+
+        batch_loss = (loss / int(targ.shape[1]))
+
+        total_loss += batch_loss
+
+        variables = encoder.variables + decoder.variables
+
+        gradients = tape.gradient(loss, variables)
+
+        optimizer.apply_gradients(zip(gradients, variables))
+
+        if batch % 100 == 0:
+            print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                         batch,
+                                                         batch_loss.numpy()))
+    # saving (checkpoint) the model every 2 epochs
+    if (epoch + 1) % 1 == 0:
+        checkpoint.save(file_prefix = checkpoint_prefix)
+
+    print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                        total_loss / N_BATCH))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+def evaluate(sentence, encoder, decoder, max_length_inp, max_length_tar):
+    attention_plot = np.zeros((max_length_tar, max_length_inp))
+
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [word2idx[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs], maxlen=max_length_inp, padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+
+    result = ''
+
+    hidden = [tf.zeros((1, units))]
+    enc_out, enc_hidden = encoder(inputs, hidden, embed_layer)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([word2idx['<BOS>']], 0)
+
+    for t in range(max_length_tar):
+        predictions, dec_hidden, attention_weights = decoder(dec_input, dec_hidden, enc_out, embed_layer)
+
+        # storing the attention weights to plot later on
+        attention_weights = tf.reshape(attention_weights, (-1, ))
+        attention_plot[t] = attention_weights.numpy()
+        # print(predictions.shape)
+        predicted_id = tf.argmax(predictions[0]).numpy()
+
+        result += idx2word[predicted_id] + ' '
+
+        if idx2word[predicted_id] == '<EOS>\n':
+            return result, sentence, attention_plot
+
+        # the predicted ID is fed back into the model
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence, attention_plot
+
+# function for plotting the attention weights
+def plot_attention(attention, sentence, predicted_sentence):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(attention, cmap='viridis')
+
+    fontdict = {'fontsize': 14}
+
+    ax.set_xticklabels([''] + sentence, fontdict=fontdict, rotation=90)
+    ax.set_yticklabels([''] + predicted_sentence, fontdict=fontdict)
+
+    plt.show()
+
+def translate(sentence, encoder, decoder, max_length_inp, max_length_tar):
+    result, sentence, attention_plot = evaluate(sentence, encoder, decoder, max_length_inp, max_length_tar)
+
+    print('Input: {}'.format(sentence))
+    print('Predicted translation: {}'.format(result))
+
+    attention_plot = attention_plot[:len(result.split(' ')), :len(sentence.split(' '))]
+    plot_attention(attention_plot, sentence.split(' '), result.split(' '))
+
+# restoring the latest checkpoint in checkpoint_dir
+# checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+#checkpoint.restore("./training_checkpoints/ckpt-6").assert_consumed()
+
+translate(u'how are you', encoder, decoder, max_length_inp, max_length_tar)
+
+while True:
+    que = input("what is your problem:- ")
+    translate(que, encoder, decoder, max_length_inp, max_length_tar)
